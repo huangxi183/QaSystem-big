c9607c3ad69daf3032623e35a5aa9d2811c4e0c8
huangxi183 <huangxi183@gmail.com>
2016-12-12 15:46:40 -0500

    fix mapper

--- src/cs.nyu.rbda.QaSystem/profile/freebase/ProFreebaseMapper.java
+++ src/cs.nyu.rbda.QaSystem/profile/freebase/ProFreebaseMapper.java
@@ -19,11 +19,10 @@ public class ProFreebaseMapper
         //Only take care of complete info in english
         if(temp.length == 8 ){
             //take subject as key, object + predicate as key
-            context.write(new Text(temp[4]), new Text(temp[6]+","+temp[5]));
+            context.write(new Text(temp[4].trim()), new Text(temp[6].trim()+","+temp[5].trim()));
             //take object as key, subject + predicate as key
-            context.write(new Text(temp[6]), new Text(temp[4]+","+temp[5]));
+            context.write(new Text(temp[6].trim()), new Text(temp[4].trim()+","+temp[5].trim()));
         }
-
     }
 }
 
